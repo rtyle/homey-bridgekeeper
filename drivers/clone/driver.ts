e7b74576c7e7004f898d69395a7c7b518ff569c8
Ross Tyler <rossetyler@gmail.com>
2026-01-30 08:10:07 -0800

    assume the class of our peer

--- drivers/clone/driver.ts
+++ drivers/clone/driver.ts
@@ -2,7 +2,7 @@ import Homey from 'homey';
 import { HomeyAPIV3Local } from 'homey-api';
 
 import type App from '../../app.ts';
-import Log from '../../lib/logger.js';
+import Logger from '../../lib/logger.js';
 import Capabilities from '../../lib/capabilities.js';
 
 interface Device extends HomeyAPIV3Local.ManagerDevices.Device {
@@ -14,14 +14,18 @@ interface Api extends HomeyAPIV3Local {
 }
 
 class Clone extends Homey.Driver {
-  protected logger = Log.get(this.constructor.name);
+  protected logger = Logger.get(this.constructor.name);
 
-  // we can pair with any device
   async onPair(session: Homey.Driver.PairSession) {
-    this.logger.logD('onPair', JSON.stringify(session));
+    this.logger.logD('onPair');
 
+    // we can pair with any device
     const api = await (this.homey.app as App).getApi() as Api;
     const devices = Object.values(await api.devices.getDevices()) as Device[];
+
+    // back-end handlers for pair/filter_devices.html front-end
+    // provide lists of system's zones and classes (list_{zones,classes}) and
+    // getters/setters for selections of such ({g,s}et_{zones,classes}).
 
     session.setHandler('list_zones', async () => {
       const list = Object.values(await api.zones.getZones()).sort((a, b) => a.name.localeCompare(b.name));
@@ -55,12 +59,14 @@ class Clone extends Homey.Driver {
       classes = list;
     });
 
+    // back-end handler list_device (system template) front-end
+    // filters devices by zones and classes
     session.setHandler('list_devices', async () => {
       this.logger.logD('onPair list_devices');
       const driverName_ = this.manifest.name[this.homey.i18n.getLanguage()] || this.manifest.name['en'];
-      // filter devices by zones and classes
       // our device will have all of our capabilities
       // plus those of its peer's that are system (sub-)capabilities
+
       const list = devices
         .filter((peer_) => !zones.length || zones.includes(peer_.zone))
         .filter((peer_) => !classes.length || classes.includes(peer_.class))
@@ -73,8 +79,10 @@ class Clone extends Homey.Driver {
             capabilities: [...(this.manifest.capabilities || []),
               ...peer.capabilities.filter((c) => Capabilities.has(c.split('.')[0]))],
             capabilitiesOptions: { ...(this.manifest.capabilitiesOptions || {}), ...peer.capabilitiesOptions },
+            class: peer.class,
           };
         });
+
       this.logger.logV('onPair list_devices', JSON.stringify(list));
       return list;
     });
